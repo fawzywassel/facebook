99695d4d056ace134f818856bc058ab6884b99a1
fawzywassel <f.wassel@pitechnologies.net>
2022-04-13 11:10:07 +0200

    mongo config

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const cors = require('cors');
 const {readdirSync} = require('fs')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config()
+
+
 const app = express();
 
 // __________ MIDDLEWARES __________ //
 app.use(cors())
+
+
+
+// ___________ ROUTES ______________//
 readdirSync("./routes").map(r=>app.use("/",require('./routes/'+r)))
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
-});
-app.get("/books", (req, res) => {
-  res.send("test");
-});
+
+// __________ DATABASE CONNECTION __________ //
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL,{},(error)=>{
+  error ? console.log(error) :console.log("DB Connection Created Successful ....")
+})
+
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
